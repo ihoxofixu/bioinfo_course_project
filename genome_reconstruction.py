7f0ca7c37472251a54617cc60f7a6e38c3d2f955
ihoxofixu <ve.gafurov@gmail.com>
2021-05-08 15:14:50 +0300

    just a try to make programm more flexible for user

--- genome_reconstruction.py
+++ genome_reconstruction.py
@@ -1,7 +1,34 @@
-from string_reconstruction import fasta_input, reconstruct_string
+import sys
+import argparse
+import string_reconstruction
 
 
-fin = open('input.txt', 'r')
-list_of_kmers = fasta_input(fin.readlines())
-print(reconstruct_string(list_of_kmers))
-fin.close()
+def main():
+    args_parser = argparse.ArgumentParser(description='This script dedicated \
+                                          to the assembling genome topic.')
+    args_parser.add_argument('--input',
+                             type=str,
+                             default='STDIN',
+                             help='Input file name or \'STDIN\'. \
+                             (Default STDIN) \
+                             Terminate input from STDIN - Ctrl-Z or Ctrl-D')
+    args = args_parser.parse_args()
+    if args.input == 'STDIN':
+        input_stream = sys.stdin
+    else:
+        try:
+            input_stream = open(args.input, 'r')
+        except(FileNotFoundError, IOError):
+            print('File not found:', args.input)
+            return
+    input_strings = input_stream.readlines()
+    list_of_kmers = string_reconstruction.clear_fasta_input(input_strings)
+    kmers_alligning, reconstructed_string = \
+        string_reconstruction.reconstruct_string(list_of_kmers)
+    for i in range(len(kmers_alligning)):
+        print(i*' ' + kmers_alligning[i])
+    print(reconstructed_string)
+
+
+if __name__ == '__main__':
+    main()
